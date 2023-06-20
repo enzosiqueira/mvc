28d5fff8525022c84ff6a485bb3a3c3773050cb2
Enzo Siqueira <enzo.siqueira01@etec.sp.gov.br>
2023-06-20 11:14:01 -0300

    Projeto MVC

---
+++ CadastoClientes/CadastoClientes/Controllers/ClienteController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastoClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ CadastoClientes/CadastoClientes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CadastoClientes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
